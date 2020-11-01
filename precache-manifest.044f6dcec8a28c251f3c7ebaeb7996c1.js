0961db51e38590a3958372e9f34d567ac1ef47d7
raghurajj <raghurajpratap1999@gmail.com>
2020-11-01 15:20:19 +0530

    Updates

---
+++ precache-manifest.044f6dcec8a28c251f3c7ebaeb7996c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73bef4c86dad5f5486882f37b4f31db4",
+    "url": "/ComplaintDesk/index.html"
+  },
+  {
+    "revision": "60599d5a06dae7658069",
+    "url": "/ComplaintDesk/static/css/2.2a1d4e3d.chunk.css"
+  },
+  {
+    "revision": "b6aff8622063a3b61532",
+    "url": "/ComplaintDesk/static/css/main.ec546ddb.chunk.css"
+  },
+  {
+    "revision": "60599d5a06dae7658069",
+    "url": "/ComplaintDesk/static/js/2.c708f150.chunk.js"
+  },
+  {
+    "revision": "6177d1ab9ae3c0c435b4d0c754a1b33b",
+    "url": "/ComplaintDesk/static/js/2.c708f150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6aff8622063a3b61532",
+    "url": "/ComplaintDesk/static/js/main.38bbcf54.chunk.js"
+  },
+  {
+    "revision": "4eb32f6bc8f75ab05c4d",
+    "url": "/ComplaintDesk/static/js/runtime-main.ca3491c3.js"
+  }
+]);
